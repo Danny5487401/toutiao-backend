a7c987aa44016f8dbf71d5f2e5981dbf2e6c5f28
xiaxin <xiaxin5487401@gmail.com>
2020-09-25 15:59:57 +0800

    "异步发送短信"

--- common/settings/default.py
+++ common/settings/default.py
@@ -1,2 +1,15 @@
 class DefaultConfig(object):
     pass
+
+#Celery配置文件
+class CeleryConfig(object):
+    """
+    Celery默认配置
+    """
+    #设置中间人 rabbitmq 指定消息队列的位置
+    broker_url = 'amqp://admin:admin@81.68.197.3:5672/my_vhost'
+
+    # task_routes = {
+    #     'sms.*': {'queue': 'sms'},
+    # }
+
