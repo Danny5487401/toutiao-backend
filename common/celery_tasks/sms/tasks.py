e367698f3c1182ab9dae7d47d3236231e9f0a9c1
danny <540021730@qq.com>
2020-10-09 11:51:53 +0800

    路由添加

--- common/celery_tasks/sms/tasks.py
+++ common/celery_tasks/sms/tasks.py
@@ -2,9 +2,9 @@
 from celery_tasks.sms.yuntongxun.sms import CCP
 from . import constants
 from celery.utils.log import get_task_logger
-tid = constants.SMS_VERIFICATION_CODE_TEMPLATE_ID
 from celery_tasks.main import app
 
+tid = constants.SMS_VERIFICATION_CODE_TEMPLATE_ID
 
 
 sms_time = constants.SMS_CODE_EXPIRES/60
@@ -16,6 +16,7 @@ sms_time = constants.SMS_CODE_EXPIRES/60
 # max_retries：异常自动重试次数的上限
 logger = get_task_logger(__name__)
 # import time
+
 
 @app.task(bind=True, name='sms.send_verification_code', retry_backoff=3)
 def send_sms_code(self,mobile,sms_code):
--- toutiao/main.py
+++ toutiao/main.py
@@ -15,4 +15,8 @@ app = create_app(DefaultConfig, enable_config_file=True)
 
 @app.route('/')
 def route_map():
-    return "hello"
+    """
+    主视图，返回所有视图网址
+    """
+    rules_iterator = app.url_map.iter_rules()
+    return jsonify({rule.endpoint: rule.rule for rule in rules_iterator if rule.endpoint not in ('route_map', 'static')})
