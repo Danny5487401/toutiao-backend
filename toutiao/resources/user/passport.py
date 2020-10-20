2705ea371d28cfa8875621ac2fbb7c182b666b50
xiaxin <xiaxin5487401@gmail.com>
2020-10-20 14:05:58 +0800

    代码冲突解决

--- toutiao/resources/user/passport.py
+++ toutiao/resources/user/passport.py
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-from flask_restful import Resource
-from utils.limiter import limiter as lmt
-from flask_limiter.util import get_remote_address
-import random
-from flask import request,current_app
-from . import constants
-from celery_tasks.sms.tasks import send_sms_code
-
-class SMSVerificationCodeResource(Resource):
-    """
-    短信验证码
-    """
-    error_message = 'Too many requests.'
-
-    decorators = [
-        lmt.limit(constants.LIMIT_SMS_VERIFICATION_CODE_BY_MOBILE,
-                  key_func=lambda: request.view_args['mobile'],
-                  error_message=error_message),
-        lmt.limit(constants.LIMIT_SMS_VERIFICATION_CODE_BY_IP,
-                  key_func=get_remote_address,
-                  error_message=error_message)
-    ]
-
-    def get(self, mobile):
-        code = '{:0>6d}'.format(random.randint(0, 999999))
-        current_app.redis_master.setex('app:code:{}'.format(mobile), constants.SMS_VERIFICATION_CODE_EXPIRES, code)
-        send_sms_code.delay(mobile, code)
-        return {'mobile': mobile}
-=======
 from flask_restful import Resource
 from utils.limiter import limiter as lmt
 from flask_limiter.util import get_remote_address
@@ -149,4 +119,3 @@ class AuthorizationResource(Resource):
         return {'token': token, 'refresh_token': refresh_token}, 201
 
 
->>>>>>> e834388b2bd5275608c69e52dec55b044d66af04
