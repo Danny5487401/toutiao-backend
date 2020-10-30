577cc815ba5564fb7b7ab7d41ec3b2555a07fbd7
danny <540021730@qq.com>
2020-10-30 13:01:50 +0800

    搜索历史增加

--- toutiao/resources/search/search.py
+++ toutiao/resources/search/search.py
@@ -67,4 +67,60 @@ class SearchResource(Resource):
         return {'total_count': total_count, 'page': page, 'per_page': per_page, 'results': results}
 
 
+class SuggestionResource(Resource):
+    """
+    联想建议
+    """
+    def get(self):
+        """
+        获取联想建议
+        """
+        qs_parser = RequestParser()
+        qs_parser.add_argument('q', type=inputs.regex(r'^.{1,50}$'), required=True, location='args')
+        args = qs_parser.parse_args()
+        q = args.q
+
+        # 先尝试自动补全建议查询
+        query = {
+            'from': 0,
+            'size': 10,
+            '_source': False,
+            'suggest': {
+                'word-completion': {
+                    'prefix': q,
+                    'completion': {
+                        'field': 'suggest'
+                    }
+                }
+            }
+        }
+        ret = current_app.es.search(index='completions', body=query)
+        options = ret['suggest']['word-completion'][0]['options']
+
+        # 如果没得到查询结果，进行纠错建议查询
+        if not options:
+            query = {
+                'from': 0,
+                'size': 10,
+                '_source': False,
+                'suggest': {
+                    'text': q,
+                    'word-phrase': {
+                        'phrase': {
+                            'field': '_all',
+                            'size': 1
+                        }
+                    }
+                }
+            }
+            ret = current_app.es.search(index='articles', doc_type='article', body=query)
+            options = ret['suggest']['word-phrase'][0]['options']
+
+        results = []
+        for option in options:
+            if option['text'] not in results:
+                results.append(option['text'])
+
+        return {'options': results}
+
 
